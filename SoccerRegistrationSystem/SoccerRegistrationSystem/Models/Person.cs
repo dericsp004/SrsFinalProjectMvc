226026899475c078e2c1959ce1bc764309aeef3a
jessi <jessi@LAPTOP-0J9K4DLL>
2017-04-01 16:16:42 -0600

    Model database created

---
+++ SoccerRegistrationSystem/SoccerRegistrationSystem/Models/Person.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SoccerRegistrationSystem.Models
+{
+    public abstract class Person
+    {
+        public int ID { get; set; }
+
+        [Required]
+        [StringLength(50, ErrorMessage = "Last name cannot be longer than 50 characters.")]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Required]
+        [StringLength(50, ErrorMessage = "First name cannot be longer than 50 characters.")]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [StringLength(50, ErrorMessage = "Middle name cannot be longer than 50 characters.")]
+        [Display(Name = "Middle Name")]
+        public string MiddleName { get; set; }
+
+        public DateTime BirthDay; 
+    }
+}
---
+++ SoccerRegistrationSystem/SoccerRegistrationSystem/Models/Player.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+namespace SoccerRegistrationSystem.Models
+{
+    public enum Position
+    {
+        NoPreference, Forward, Midfield, Defense, GoalKeeper
+    }
+
+    public class Player : Person
+    {
+        public int YearsExperience { get; set; }
+        public Position? PreferredPosition { get; set; }
+        [Required]
+        [StringLength (100, ErrorMessage ="Guardian Name Length cannot be longer than 100 characters")]
+        [Display(Name = "Parent/Guardian Name")]
+        public string GuardianName { get; set; }
+
+    }
+}
